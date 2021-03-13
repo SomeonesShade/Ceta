48f9a55fd56d9576eb47621c6efc182683b94caa
Shade <jomil.paulo@gmail.com>
2021-03-13 21:26:06 +0800

    1Commit

---
+++ Assets/C#Scripts/CameraStabilizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStabilizer : MonoBehaviour
+{
+    private Transform tm;
+    private Quaternion StartRotation;
+    void Awake()
+    {
+        tm = GetComponent<Transform>();
+        StartRotation = tm.rotation;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        tm.rotation = StartRotation
+    }
+}
