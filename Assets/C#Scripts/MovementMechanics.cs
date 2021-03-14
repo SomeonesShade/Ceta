413ba88e3529c4aef7479ef6c3acd4caf60f37f3
Shade <jomil.paulo@gmail.com>
2021-03-14 15:48:22 +0800

    First Actuall Commit

--- Assets/C#Scripts/MovementMechanics.cs
+++ Assets/C#Scripts/MovementMechanics.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MovementMechanics : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float speedMultiplier;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");\
+        rb.velocity = new Vector(
+            movement.x*speedMultiplier,
+            movement.y*speedMultiplier);jj
     }
 }
--- Assets/C#Scripts/RotationMechanics.cs
+++ Assets/C#Scripts/RotationMechanics.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class RotationMechanics : MonoBehaviour
 {
+    private Transform tm;
+    private Vector2 mouseLocation;
+    private Vector2 relPos;
+    private float angle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tm = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        relPos = mouseLocation - new Vector2(tm.position.x, tm.position,y)
+        angle = Mathf.Atan2(relPos.y, relPos.x) * Mathf.Rad2Deg;
+        tm.rotation = Quaternion.Euler(0,0,angle);
     }
 }
